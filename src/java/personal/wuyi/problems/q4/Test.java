a82205c138eb412420acf55c5448a148284b9d8e
wuyichen24 <wuyichen24@gmail.com>
2019-04-11 11:25:01 -0700

    add question 4

---
+++ src/java/personal/wuyi/problems/q4/Test.java
@@ -0,0 +1,11 @@
+package personal.wuyi.problems.q4;
+
+public class Test {
+	public static void main(String[] args) {
+		int[] nums1 = new int[] {1, 2, 3, 4, 5};
+		int[] nums2 = new int[] {6};
+		MySolution my = new MySolution();
+		double result = my.findMedianSortedArrays(nums1, nums2);
+		System.out.println(result);
+	}
+}
