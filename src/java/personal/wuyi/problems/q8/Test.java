2d9ec80f86c6949883b5bd4f449972cfcbe6dde7
wuyichen24 <wuyichen24@gmail.com>
2019-04-13 20:38:43 -0700

    Add question 8

---
+++ src/java/personal/wuyi/problems/q8/Test.java
@@ -0,0 +1,10 @@
+package personal.wuyi.problems.q8;
+
+public class Test {
+
+	public static void main(String[] args) {
+		MySolution my  = new MySolution();
+		System.out.println(my.myAtoi("  -42"));
+	}
+
+}
