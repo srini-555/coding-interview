6cecab9fbd5c5d1af743d2cd71b914b41c00ee59
wuyichen24 <wuyichen24@gmail.com>
2019-04-15 21:23:43 -0700

    Add question 13 and question 14

---
+++ src/java/personal/wuyi/problems/q14/MySolution.java
@@ -0,0 +1,31 @@
+package personal.wuyi.problems.q14;
+
+public class MySolution {
+	public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        
+        String prefix = "";
+        
+        for (int i = 1; i < 1000; i++) {
+            if (i <= strs[0].length()) {
+                String currentPrefix = strs[0].substring(0, i);
+                
+                for (int j = 1; j < strs.length; j++) {
+                    if (strs[j].length() < i) {
+                        return prefix;
+                    } else if (!strs[j].substring(0, i).equals(currentPrefix)) {
+                        return prefix;
+                    }
+                }
+                
+                prefix = currentPrefix;
+            } else {
+                return prefix;
+            }
+        }
+        
+        return prefix;
+    }
+}
